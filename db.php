bd391d304d3104a1808e4aa115c87423ab89fa15
siripak paitoon <aif912752@gmail.com>
2023-09-16 20:08:44 +0700

    first commit

--- db.php
+++ db.php
@@ -11,10 +11,6 @@ try {
     // Set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // You can optionally set character encoding here, if needed
-    // For example, if you are using utf8, you can add this line:
-    // $conn->exec("SET NAMES 'utf8'");
-
 } catch (PDOException $e) {
     // If there is an error in the connection, catch it and display the error message
     echo "Connection failed: " . $e->getMessage();
